4b8f2dd60a50e3c4c9ba2106c8bf942829dce643
ThanhBD <buiducthanh1593@gmail.com>
2023-10-31 09:21:12 +0700

    upload file

--- setup.py
+++ setup.py
@@ -16,9 +16,10 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         'einops==0.2.0',
-        'gdown==4.4.0',
+        'scikit-image>=0.21.0',
         'prefetch_generator==1.0.1',
-        'lmdb>=1.0.0'
+        'lmdb>=1.0.0',
+        'imgaug==0.4.0'
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
